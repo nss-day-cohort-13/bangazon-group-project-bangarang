e37f53794c806c74800e2726ec08eb2fd09f631f
Benjamin Wilburn <bwilburnstrength@gmail.com>
2016-08-08 15:21:27 -0500

    added inital menu

---
+++ menu.py
@@ -0,0 +1,22 @@
+from bangazon import *
+def show_initial_menu():
+    print()
+    print('1.) Create A Customer Account')
+    print()
+    print('2.) Choose An Active Customer')
+    print()
+    print('3.) Create A Payment Option')
+    print()
+    print('4.) Add Product To Shopping Cart')
+    print()
+    print('5.) Complete An Order')
+    print()
+    print('6.) See Product Popularity')
+    print()
+    print('7.) Leave Bangazon')
+    selection = input('Show me the money >>>  ')
+
+    
+
+if __name___ == "__main__":
+    show_initial_menu()
