d2c928972be25a1bf668304211db12e490a4e3cc
slick <pbhowelljr@gmail.com>
2016-08-09 10:06:43 -0500

    refactor payment options class creation

--- test_payment_options.py
+++ test_payment_options.py
@@ -6,13 +6,15 @@ class TestPayment(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        self.payment = PaymentOption("Visa", 123456789)
+        self.payment = PaymentOption("Visa", 123456789, 23456789)
 
     def test_create_payment(self):
         self.assertEqual(self.payment.name, "Visa")
         self.assertEqual(self.payment.account_number, 123456789)
-        self.assertIsNotNone(self.payment.payment_option_id)
-        self.assertIsInstance(self.payment.payment_option_id, int)
+        self.assertIsNotNone(self.payment.obj_id)
+        self.assertIsInstance(self.payment.obj_id, int)
+        self.assertIsNotNone(self.payment.customer_id)
+        self.assertIsInstance(self.payment.customer_id, int)
 
 
 if __name__ == '__main__':
