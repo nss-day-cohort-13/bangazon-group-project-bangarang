1f47ba6e241fef9c834e4875627023f8026dd92f
slick <pbhowelljr@gmail.com>
2016-08-09 02:18:09 -0500

    refactor main menu functionality

--- menu.py
+++ menu.py
@@ -1,22 +1,13 @@
 from bangazon import *
-def show_initial_menu():
-    print()
-    print('1.) Create A Customer Account')
-    print()
-    print('2.) Choose An Active Customer')
-    print()
-    print('3.) Create A Payment Option')
-    print()
-    print('4.) Add Product To Shopping Cart')
-    print()
-    print('5.) Complete An Order')
-    print()
-    print('6.) See Product Popularity')
-    print()
-    print('7.) Leave Bangazon')
-    selection = input('Show me the money >>>  ')
+def generate_main_menu():
+    output = '\n 1. Create A Customer Account' +
+    '\n 3. Create A Payment Option' +
+    '\n 4. Add Product To Shopping Cart' +
+    '\n 5. Complete An Order' +
+    '\n 6. See Product Popularity' +
+    '\n 7. Leave Bangazon'
+    return output
 
-    
 
 if __name___ == "__main__":
     show_initial_menu()
