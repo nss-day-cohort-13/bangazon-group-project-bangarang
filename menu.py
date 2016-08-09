d0165c43812555f0957bd0f12c3bfc394b23614e
slick <pbhowelljr@gmail.com>
2016-08-09 02:19:27 -0500

    add logic for main application menu

--- menu.py
+++ menu.py
@@ -9,5 +9,24 @@ def generate_main_menu():
     return output
 
 
-if __name___ == "__main__":
-    show_initial_menu()
+def runner():
+    print(generate_main_menu())
+    print('\n Input option number:')
+    user_input = input('\n > ')
+    if user_input == '1':
+        run_create_user()
+    elif user_input == '2':
+        run_select_user()
+    elif user_input == '3':
+        run_create_payment()
+    elif user_input == '4':
+        run_add_products()
+    elif user_input == '5':
+        run_complete_order()
+    elif user_input == '6':
+        run_generate_popularity_report()
+    elif user_input == '7':
+        pass
+
+if __name__ == '__main__':
+    runner()
